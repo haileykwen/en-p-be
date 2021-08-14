21773f7e41c92e8b51fff4f4ebb657fd45dd0708
harleykwen <harli.ramli@erasysconsulting.com>
2021-08-15 02:41:28 +0700

    fix some issue

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3001;
+const cors = require('cors');
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
 
